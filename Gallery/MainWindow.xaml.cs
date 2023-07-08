c0a13ce4d452de987abdf5bcb6d71b8608bfcb6c
CrashTagiyev <121178020+CrashTagiyev@users.noreply.github.com>
2023-07-08 16:48:21 +0300

    v 1.2.0.0

--- Gallery/MainWindow.xaml.cs
+++ Gallery/MainWindow.xaml.cs
@@ -18,21 +18,47 @@ using System.Drawing;
 using System.Reflection.Metadata;
 using Bogus;
 using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Windows.Threading;
 
 namespace Gallery
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        private DispatcherTimer playTimer;
+        private int currentIndex;
         public List<ImageClass>? MyImages { get; set; }
         public MainWindow()
         {
             InitializeComponent();
             DataContext = this;
-            MyImages = new Faker<ImageClass>().RuleFor(ic => ic.ImageUrl, faker => faker.Image.PicsumUrl()).Generate(20);
+            MyImages = new Faker<ImageClass>().RuleFor(ic => ic.ImageUrl, faker => faker.Image.PicsumUrl()).Generate(5);
+            ImageList.SelectedIndex = -1;
+
+            playTimer = new DispatcherTimer();
+            playTimer.Interval = TimeSpan.FromSeconds(1);
+            playTimer.Tick += new EventHandler(PlayTimer_Tick);
         }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public event PropertyChangingEventHandler? PropertyChanging;
+
+        void INotifyPropertyChanged([CallerMemberName] string propName = "") => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+
+
+        private int imageSize;
+
+        public int ImageSize
+        {
+            get { return imageSize; }
+
+            set { imageSize = value; INotifyPropertyChanged(); }
+        }
+
 
         private void ImageList_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
@@ -42,37 +68,82 @@ namespace Gallery
             ShowImg.Source = imageSource;
         }
 
+
+
+
+
+
+
+       
+
         private void CheckBox_Checked(object sender, RoutedEventArgs e)
         {
             try
             {
-                
+
+                Binding binding = new Binding();
+                binding.Source = sldWith;
+                binding.Path = new PropertyPath("Value");
+                binding.Mode = BindingMode.TwoWay;
+                BindingOperations.SetBinding(sldHeight, Slider.ValueProperty, binding);
             }
             catch (Exception ex)
             {
 
                 MessageBox.Show(ex.Message);
             }
+
         }
 
-      
-
-        
-
-        private void sldWith_PreviewMouseMove(object sender, MouseEventArgs e)
+        private void Prev_Button_Click(object sender, RoutedEventArgs e)
         {
-            if (BothAuto.IsChecked == true)
+            if (ImageList.SelectedIndex - 1 > -1)
             {
-                sldHeight.Value = sldWith.Value;
+                ImageClass? ic = ImageList.Items[ImageList.SelectedIndex - 1] as ImageClass;
+                ImageList.SelectedIndex -= 1;
+                Uri? imageUri = new Uri(ic.ImageUrl, UriKind.RelativeOrAbsolute);
+                ImageSource imageSource = new BitmapImage(imageUri);
+                ShowImg.Source = imageSource;
             }
         }
 
-        private void sldHeight_PreviewMouseMove(object sender, MouseEventArgs e)
+        private void Next_Button_Click(object sender,RoutedEventArgs e)
         {
-            if (BothAuto.IsChecked == true)
+            if (ImageList.SelectedIndex + 1 < ImageList.Items.Count)
             {
-                sldWith.Value = sldHeight.Value;
+                ImageClass? ic = ImageList.Items[ImageList.SelectedIndex + 1] as ImageClass;
+                ImageList.SelectedIndex += 1;
+                Uri? imageUri = new Uri(ic.ImageUrl, UriKind.RelativeOrAbsolute);
+                ImageSource imageSource = new BitmapImage(imageUri);
+                ShowImg.Source = imageSource;
             }
+        }
+
+        private void Play_Button_Click(object sender, RoutedEventArgs e)
+        {
+            playTimer.Start();
+        }
+        private void PlayTimer_Tick(object sender, EventArgs e)
+        {
+            if (currentIndex < ImageList.Items.Count - 1)
+            {
+                currentIndex++;
+            }
+            else
+            {
+                currentIndex = 0;
+            }
+
+            ImageClass? ic = ImageList.Items[currentIndex] as ImageClass;
+            ImageList.SelectedIndex = currentIndex;
+            Uri? imageUri = new Uri(ic.ImageUrl, UriKind.RelativeOrAbsolute);
+            ImageSource imageSource = new BitmapImage(imageUri);
+            ShowImg.Source = imageSource;
+        }
+
+        private void Stop_Button_Click(object sender, RoutedEventArgs e)
+        {
+            playTimer.Stop();
         }
     }
 }
